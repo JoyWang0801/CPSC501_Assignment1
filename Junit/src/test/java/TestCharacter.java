82b65a6f23250ba53c7217ec7dc3d5665f5ffb7d
Joy Wang <yihan.wang3@ucalgary.ca>
2023-10-04 23:59:26 -0600

    added some junit tests for testing character's special attack

--- Junit/src/test/java/TestCharacter.java
+++ Junit/src/test/java/TestCharacter.java
@@ -2,22 +2,47 @@ package Junit.src.test.java;
 
 import Chara.*;
 import Chara.Character;
+import main.*;
+import main.mapGenerator;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class TestCharacter {
 
     static ArrayList<Character> character_list;
-    int list_size;
+    static ArrayList<Character> enemy_list;
+    int character_list_size;
 
+    static map m;
+
+
+    /*
+    private static void putPawnsOnMap()
+    {
+        int[] test = m.getCharPos();// get default instatiated character position list
+        int[] test2 = m.getEnemyPos();// get default enemy posiyions
+        for(int i = 0; i < 3; i++) {//for all player characters
+            int index = i * 2;//get an index to use for the 1-D list of deafult positions (goes x then y)
+            m.setPos(players.get(i).getID(), m.getCharPos()[index], m.getCharPos()[index + 1]);//set the id of that character on the map
+        }
+
+        m.setPos(enemies.getID(), m.getEnemyPos()[0], m.getEnemyPos()[1]);
+
+        m.setPos(7, m.getItemPos()[0], m.getItemPos()[1]);//set the id of an item on the map (only one for now - will generate a random number when new items are eventually added
+    }
+*/
     @BeforeClass
     public static void SetUpClass()
     {
+        mapGenerator mg = new mapGenerator();
+        m = mg.generate();
+
         int i = 0;
         character_list  = new ArrayList<>();
 
@@ -27,26 +52,33 @@ public class TestCharacter {
         character_list.add(testCharacter);
         testCharacter = new EngineeringMajor(i++);
         character_list.add(testCharacter);
+
         testCharacter = new ChemistryMajor(i++);
         character_list.add(testCharacter);
         testCharacter = new BiomedicalMajor(i++);
         character_list.add(testCharacter);
-        testCharacter = new PhilosophyMajor(i);
+        testCharacter = new PhilosophyMajor(i++);
         character_list.add(testCharacter);
+
+        enemy_list = new ArrayList<>();
+        Character enemy = new Enemy("Enemy", i++, 15, 150, 0, 15, 150, 0, 3, 1);
+        enemy_list.add(enemy);
+        enemy = new Enemy("Enemy", i, 15, 150, 0, 15, 150, 0, 3, 1);
+        enemy_list.add(enemy);
     }
 
     @Before
     public void SetUpTest()
     {
         // making sure all characters are initiated
-        list_size = character_list.size();
-        assertEquals(6, list_size);
+        character_list_size = character_list.size();
+        assertEquals(6, character_list_size);
     }
 
     @Test
     public void TestSetAndGetID()
     {
-        for (int i = 0; i < list_size; i++)
+        for (int i = 0; i < character_list_size; i++)
         {
             // verify current id
             assertEquals(i, character_list.get(i).getID());
@@ -233,5 +265,38 @@ public class TestCharacter {
             assertEquals(msg, victim_health - attacker_attack, victim.getHealth());
             assertEquals(msg, victim_health - victim.getHealth(), attacker_attack);
         }
+    }
+
+
+    @Test
+    public void testBiomedicalSpecial()
+    {
+        Character current_character = character_list.get(0);
+        Character vitim = enemy_list.get(0);
+        int character_id = current_character.getID();
+        int victim_xpos = m.getPos(character_id)[0];
+        int victim_ypos = m.getPos(character_id)[1] + 1;
+        m.setPos(vitim.getID(), victim_xpos, victim_ypos);
+        int new_health = vitim.getHealth() - current_character.getAttack() * 2;
+
+        boolean success = current_character.Special(m, character_list, enemy_list, victim_xpos, victim_ypos);
+
+        assertEquals(new_health, vitim.getHealth());
+    }
+
+    @Test
+    public void testZoologySpecial()
+    {
+        Character current_character = character_list.get(1);
+        int character_id = current_character.getID();
+        m.setPos(character_id, 6, 6);
+
+        int new_x = m.getPos(character_id)[0];
+        int new_y = m.getPos(character_id)[1] + 1;
+
+        boolean success = current_character.Special(m, character_list, enemy_list, new_y, new_x);
+
+        assertEquals(new_x, m.getPos(character_id)[0]);
+        assertEquals(new_y, m.getPos(character_id)[1]);
     }
 }
